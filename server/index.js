9f892f5b59bcb6e3165f069554ac5f37fb0c7bbf
JitendraKumar1092 <jitendra.kumar_cs20@gla.ac.in>
2023-11-27 00:55:51 +0530

    added some new features like downloading and sharing

--- server/index.js
+++ server/index.js
@@ -14,8 +14,7 @@ app.use("/api/v1/imaginary", imaginaryRoutes);
 app.get("/", async (req, res) => {
   res.send("hello from Imaginary");
 });
-const url =
-  "mongodb+srv://joker:1234567890@cluster0.gyhebku.mongodb.net/?retryWrites=true&w=majority";
+const url = process.env.MONGO_URL;
 const startServer = async () => {
   try {
     connectDB(url);
